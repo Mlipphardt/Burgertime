268ece7f7ceb0fb3dfff6db6f6d57ea03f261c3c
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-20 22:11:50 -0400

    Adjusted README, added delete button, removed test logging

--- public/assets/js/burgerbuttons.js
+++ public/assets/js/burgerbuttons.js
@@ -1,67 +1,56 @@
-$(function() {
-    
-    $("#create-burger-button").on("click", function(event) {
-        event.preventDefault();
-        if ($("#burger").val().trim() === ""){
-            console.log("Error - no name provided.")
-        } else{ 
+$(function () {
+  //When clicked, insert a new burger into the MySQL burgers table.
+  $("#create-burger-button").on("click", function (event) {
+    event.preventDefault();
+    if ($("#burger").val().trim() === "") {
+      console.log("Error - no name provided.");
+    } else {
+      var newBurger = {
+        name: $("#burger").val().trim(),
+        devoured: false,
+      };
 
-            var newBurger = {
-            name: $("#burger").val().trim(),
-            devoured: false
-            };
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger,
+      }).then(function () {
+        location.reload();
+      });
+    }
+  });
 
-            console.log(newBurger);
+  //When clicked, update the corresponding entry in the burgers table
+  //to indicate the burger has been eaten.
+  $(".eat-burger").on("click", function (event) {
+    event.preventDefault();
+    let id = $(this).attr("data-id");
 
-            $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-            }).then(
-            function() {
-                location.reload();
-            }
-            );
-        }
+    let burgerEaten = {
+      devoured: 1,
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: burgerEaten,
+    }).then(function () {
+      location.reload();
     });
+  });
 
-    $(".eat-burger").on("click", function(event){
-        event.preventDefault();
-        let id = $(this).attr("data-id");
-        console.log(id)
+  //When clicked, return an eaten burger to the 'ready' column.
+  $(".renew-burger").on("click", function (event) {
+    event.preventDefault();
+    let id = $(this).attr("data-id");
 
-        let burgerEaten = {
-            devoured: 1
-        }
+    let burgerRenewed = {
+      devoured: 0,
+    };
 
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: burgerEaten
-        }).then(
-            function() {
-                location.reload();
-            }
-        );
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: burgerRenewed,
+    }).then(function () {
+      location.reload();
     });
-
-    $(".renew-burger").on("click", function(event){
-        event.preventDefault();
-        let id = $(this).attr("data-id");
-        console.log(id)
-
-        let burgerRenewed = {
-            devoured: 0
-        }
-
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: burgerRenewed
-        }).then(
-            function() {
-                location.reload();
-            }
-        );
-    });
-    
-    
-
+  });
 });
