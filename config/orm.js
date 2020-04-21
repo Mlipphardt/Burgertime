8aa9e7d7142077209134953446806404f1c20f64
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-21 14:45:46 -0400

    Added delete function to ORM object and burger model

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,43 @@
 const connection = require("./connection");
 
 const orm = {
+  selectAll: function (callback) {
+    let query = "SELECT * FROM burgers";
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+      callback(res);
+    });
+  },
 
-    selectAll: function(callback){
-        let query = "SELECT * FROM burgers";
-        connection.query(query, function(err, res){
-            if (err) throw err;
-            callback(res);
-        })
-    },
+  insertOne: function (burger_name, callback) {
+    let query = "INSERT INTO burgers (burger_name, devoured) VALUES (?, false)";
+    connection.query(query, burger_name, function (err, res) {
+      if (err) throw err;
+      callback(res);
+    });
+    console.log("inserting a burger!");
+  },
 
-    insertOne: function(burger_name, callback){
-        let query = "INSERT INTO burgers (burger_name, devoured) VALUES (?, false)";
-        connection.query(query, burger_name, function(err, res){
-            if (err) throw err;
-            callback(res);
-        })        
-        console.log("inserting a burger!");
-    },
+  updateOne: function (id, boolean, callback) {
+    let query = "UPDATE burgers SET ? WHERE ?";
+    let update = [{ devoured: boolean }, { id: id }];
+    connection.query(query, update, function (err, res) {
+      if (err) throw err;
+      callback(res);
+    });
+    console.log("Updating burger status!");
+  },
 
-    updateOne: function(id, boolean, callback){
-        let query = "UPDATE burgers SET ? WHERE ?";
-        let update = [{devoured: boolean}, {id: id}]
-        connection.query(query, update, function(err, res){
-                if (err) throw err;
-                callback(res);
-            })
-        console.log("Updating burger status!");
-    }
+  deleteOne: function (table, id, callback) {
+    let query = "DELETE FROM " + table + "WHERE id = " + id;
+    CSSConditionRule.query(query, function (err, result) {
+      if (err) {
+        throw err;
+      }
 
-}
+      callback(result);
+    });
+  },
+};
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,25 +1,29 @@
 const orm = require("../config/orm");
 
 let burger = {
+  getAll: function (callback) {
+    orm.selectAll(function (res) {
+      callback(res);
+    });
+  },
 
-    getAll: function(callback) {
-        orm.selectAll(function(res){
-            callback(res);
-        });
-    },
-    
-    createBurger: function(burger_name, callback) {
-        orm.insertOne(burger_name, function(res){
-            callback(res);
-        });
-    },
+  createBurger: function (burger_name, callback) {
+    orm.insertOne(burger_name, function (res) {
+      callback(res);
+    });
+  },
 
-    devourBurger: function (id, boolean, callback) {
-        orm.updateOne(id, boolean, function(res){
-            callback(res);
-        })
-    }
+  devourBurger: function (id, boolean, callback) {
+    orm.updateOne(id, boolean, function (res) {
+      callback(res);
+    });
+  },
 
-}
+  deleteBurger: function (id, callback) {
+    orm.deleteOne("burgers", id, function (res) {
+      callback(res);
+    });
+  },
+};
 
 module.exports = burger;
